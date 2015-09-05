baea64eb543c428696dbafb54bd6b242a190d43f
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-09-05 11:49:21 -0700

    Update FindDups.py

--- primitive_datatypes/FindDups.py
+++ primitive_datatypes/FindDups.py
@@ -1,15 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
 def findDups(A):
-	for i in xrange(0,len(A)):
-		curr = i
-		while True:
-			next = A[curr]
-			tmp = A[next]
-			if next == i:
-				break
-			if next == curr:
-				print " Found Dup %s" % next
-				return next
-			A[next] = A[curr]
-			curr = tmp
-A=[1,2,3,4,4]
+    for i in xrange(0, len(A)):
+        curr = i
+        while True:
+            next = A[curr]
+            tmp = A[next]
+            if next == i:
+                break
+            if next == curr:
+                print ' Found Dup %s' % next
+                return next
+            A[next] = A[curr]
+            curr = tmp
+
+
+A = [1, 2, 3, 4, 4]
 findDups(A)
+
+			
