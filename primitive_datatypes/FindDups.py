ce5e65be67177e2ac3ec9d96cb1918a1850e3727
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-09-04 20:35:32 -0700

    Create FindDups.py

---
+++ primitive_datatypes/FindDups.py
@@ -0,0 +1,15 @@
+def findDups(A):
+	for i in xrange(0,len(A)):
+		curr = i
+		while True:
+			next = A[curr]
+			tmp = A[next]
+			if next == i:
+				break
+			if next == curr:
+				print " Found Dup %s" % next
+				return next
+			A[next] = A[curr]
+			curr = tmp
+A=[1,2,3,4,4]
+findDups(A)
