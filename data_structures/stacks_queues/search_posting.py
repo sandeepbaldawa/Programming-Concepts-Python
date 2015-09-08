05b5871f291b22b23b579bf6ec2567a2347d9702
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-09-08 08:43:08 -0700

    Create search_posting.py

---
+++ data_structures/stacks_queues/search_posting.py
@@ -0,0 +1,5 @@
+# One way to traverse a linkedlist is using the next pointer
+# Another way can be using the jump/random node
+# Each node in the linkedlist contains a random pointer which can point to any node(including itself)
+
+#Question:- How do you traverse a Linked list using next and random pointer?
