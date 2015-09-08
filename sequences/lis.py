4c4de0784fd8a4b4db58257f62b7b2b69f99c56d
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-09-08 09:54:08 -0700

    Create lis.py

---
+++ sequences/lis.py
@@ -0,0 +1,4 @@
+# Longest increasing subsequece
+# https://en.wikipedia.org/wiki/Patience_sorting
+# Patience sorting is a sorting algorithm, based on a solitaire card game, that has the property of being able 
+# to efficiently compute the length of a longest increasing subsequence in a given array.
