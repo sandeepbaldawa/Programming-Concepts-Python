e3c1a4dc377a78af011cadd7b4e6c8404bad40d2
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-09-13 19:44:46 -0700

    Add

--- trees/getMax.py
+++ trees/getMax.py
@@ -1,9 +1,4 @@
-class Node():
-    def __init__(self, data, left=None, right=None):
-        self.data = data
-        self.left = left
-        self.right = right
-
+from print_tree import *
 
 def getMax(node, max=0):
     if not node:
@@ -11,14 +6,8 @@ def getMax(node, max=0):
 
     max = getMax(node.left, max)
     max = getMax(node.right, max)
-    return max if max > node.data else node.data
+    return max if max > node.entry else node.entry
 
 
-myTree = Node(5)
-myTree.left = Node(10)
-myTree.right = Node(5)
-myTree.left.left = Node(1000)
-myTree.left.right = Node(3000)
-myTree.right.left = Node(2000)
-myTree.right.right = Node(4000)
-print getMax(myTree)
+t = BinTree(6, BinTree(2, BinTree(0), BinTree(4)), BinTree(10, BinTree(8), BinTree(12)))
+print getMax(t)
