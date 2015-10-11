e7760242d631791673c524a018ef19f45ddef2b9
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-10-10 23:24:43 -0700

    Update regex_matching.py

--- recursion/regex_matching.py
+++ recursion/regex_matching.py
@@ -1,4 +1,5 @@
 # http://articles.leetcode.com/2011/09/regular-expression-matching.html
+# If there is a "+" compare first character(d0es not match return Fale) and then replace with * and continue same logic"
 def match(string, pattern):
     if len(pattern) == 0 and len(string) == 0:
         return True
