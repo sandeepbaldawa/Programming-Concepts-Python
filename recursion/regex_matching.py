f5dc196cd6e8e1b1af248bb8c817507469af1dd3
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-10-10 23:02:45 -0700

    Update regex_matching.py

--- recursion/regex_matching.py
+++ recursion/regex_matching.py
@@ -1,1 +1,21 @@
 # http://articles.leetcode.com/2011/09/regular-expression-matching.html
+def match(string, pattern):
+    if len(pattern) == 0 and len(string) == 0:
+        return True
+
+    if (len(pattern) == 0 and len(string) > 0) or (len(pattern) > 0 and len(string) == 0):
+        return False
+
+    if pattern[0] == "*":
+        return match(string[1:], pattern[1:]) or match(string[1:], pattern)
+    elif (pattern[0] == string[0]) or ((pattern[0] == ".") and string[0]):
+        return match(string[1:], pattern[1:])
+    else:
+        return False
+
+print match("abracadabra", "ab*ca*br*")
+print match("abbb", "a.*b")
+print match("abbb", "a.*bc")
+print match("a","")
+print match("","a")
+print match("","")
