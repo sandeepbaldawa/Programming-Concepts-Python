ee0003bfe255680a08aee0f2fe670020f5f6fc1e
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-10-12 17:07:54 -0700

    Create lru_cache.py

---
+++ data_structures/lru_cache.py
@@ -0,0 +1,5 @@
+# Implement LRU Cache 
+# Idea is we should optimize both on reads and writes
+# For Reads O(1) as always Hash is the best
+# For optimized on writes to evict the LRU double linked list can be used
+# This gives O(1) for both operations
