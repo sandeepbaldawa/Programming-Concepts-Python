36cf0da0ec32b4e6c601aa587c83d0ee67459e03
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-10-15 14:10:31 -0700

    Update copy_linkedlist_w_random_pointer.py

--- data_structures/linked_lists/copy_linkedlist_w_random_pointer.py
+++ data_structures/linked_lists/copy_linkedlist_w_random_pointer.py
@@ -1,1 +1,10 @@
+1st Method
+---------------
+Create a HashMap of Key/val corresponding to the node addresses in new and old linked_list in first pass
+Then in second pass link the random nodes using row in the HashMap
+
+2nd Method
+---------------
+Inser new nodes in between the old linked list with old linked list pointing to new nodes.
+p1.next.random = p1.random.next
 
