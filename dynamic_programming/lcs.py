a8f93f68311766462e78e0f3e9283700afd94cdd
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-10-16 23:13:47 -0700

    Update lcs.py

--- dynamic_programming/lcs.py
+++ dynamic_programming/lcs.py
@@ -2,3 +2,7 @@
 # Input   :- A[1..M],B[1..N]
 # Output  :- C[1..P]
 # Subsequence of A and B
+# X is a subsequence of Y if X is obtained by dropping elements of Y
+# "ad" is a subsequence of "abcde"
+# A="abcde" and B="bceadq"  => "bcd" and "ad" are common sequences
+# LCS of A and B is "bcd"
