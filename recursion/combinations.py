3f4c455b6dcb921750d3048244a43067f518d884
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-10-19 20:51:24 -0700

    Update combinations.py

--- recursion/combinations.py
+++ recursion/combinations.py
@@ -1,3 +1,7 @@
 # Given N things how many ways can you choose K things?
 # You cannot repeat the same items
 # N Choose K written as C(N,K)
+# Lets say "Bob" is part of the group
+# Number of way to surely pick Bob C(N-1,K-1)   ===> A
+# No of ways to not include Bob in what we pick C(N-1,K) ===> B
+# Total ways are A + B
