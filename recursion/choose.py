873ba7a9cefee0e69aca8bd54c7c97137a458cf5
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-10-19 20:57:39 -0700

    Update and rename combinations.py to choose.py

--- recursion/choose.py
+++ recursion/choose.py
@@ -5,3 +5,11 @@
 # Number of way to surely pick Bob C(N-1,K-1)   ===> A
 # No of ways to not include Bob in what we pick C(N-1,K) ===> B
 # Total ways are A + B
+
+def Choose(N, K):
+    if N==K or K==0:
+        return 1
+    else:
+        return Choose(N-1, K-1) + Choose(N, K-1)
+
+print Choose(10,3)
