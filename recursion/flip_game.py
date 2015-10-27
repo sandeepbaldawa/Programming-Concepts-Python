1132707771d61e9ad3b70e2fb56c843c9b55ce38
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-10-27 09:05:06 -0700

    Update flip_game.py

--- recursion/flip_game.py
+++ recursion/flip_game.py
@@ -5,4 +5,20 @@ The game ends when a person can no longer make a move and therefore the other pe
 Write a function to determine if the starting player can guarantee a win.
 For example, given s = "++++", return true. The starting player can guarantee a win by flipping the middle "++" to become "+--+".
 """
+class Solution(object):
+    def __init__(self):
+        self.dict = {}
 
+    def canWin(self, s):
+        if s in self.dict:
+            return self.dict[s]
+        result = False
+        for i in range(len(s)-1):
+            if s[i:i+2] == "++" and not self.canWin(s[:i] + "--" + s[i+2:]):
+                result = True
+                break
+        self.dict[s] = result
+        return result
+sol = Solution()
+print sol.canWin("++++++")
+print sol.canWin("++")
