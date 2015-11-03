473e903c8ab6f9f413cd6c38d7d9f0011c9aa5fa
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-11-02 20:55:03 -0800

    Create letter_phone.py

---
+++ recursion/letter_phone.py
@@ -0,0 +1,18 @@
+"""
+LETTERPHONE
+
+Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+
+
+The digit 0 maps to 0 itself.
+The digit 1 maps to 1 itself.
+
+Input: Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Make sure the returned strings are lexicographically sorted.
+
+"""
+
