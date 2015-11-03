8b1e7ed12f896d8b52a7e812862ca7029be10a5d
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-11-02 20:57:55 -0800

    Update letter_phone.py

--- recursion/letter_phone.py
+++ recursion/letter_phone.py
@@ -15,4 +15,26 @@ Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 Make sure the returned strings are lexicographically sorted.
 
 """
+# Tail recursion
+d = { '1': "1",
+      '2': "ABC",
+      '3': "DEF",
+      '4': "GHI",
+      '5': "JKL",
+      '6': "MNO",
+      '7': "PQRS",
+      '8': "TUV",
+      '9': "WXYZ",
+}
 
+def getphoneMneumonicHelper(input, digitIndex, acc):
+	if digitIndex < len(input):
+		digit = input[digitIndex]
+		for c in d[str(ord(input[digitIndex]) - ord('0'))]:
+			acc[digitIndex] =  c
+			getphoneMneumonicHelper(input, digitIndex + 1, acc)
+	else:
+		print acc
+	
+acc = [None] * 3
+getphoneMneumonicHelper("567", 0 , acc)
