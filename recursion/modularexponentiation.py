e5e2af2dc8ff0085f2528dde0a2d2ebb2b5d31eb
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-11-02 20:44:06 -0800

    Create modularexponentiation.py

---
+++ recursion/modularexponentiation.py
@@ -0,0 +1,11 @@
+# https://youtu.be/nO7_qu2kd1Q
+"""
+Implement pow(A, B) % C.
+
+In other words, given A, B and C, 
+find (AB)%C.
+
+Input : A = 2, B = 3, C = 3
+Return : 2 
+2^3 % 3 = 8 % 3 = 2
+"""
