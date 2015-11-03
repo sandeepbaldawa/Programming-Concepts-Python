e5f9eadd058dac1c92ed83636f700f85e165d6ba
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-11-02 20:49:18 -0800

    Update modularexponentiation.py

--- recursion/modularexponentiation.py
+++ recursion/modularexponentiation.py
@@ -8,4 +8,9 @@ find (AB)%C.
 Input : A = 2, B = 3, C = 3
 Return : 2 
 2^3 % 3 = 8 % 3 = 2
+
+
+X ^ N % M = ((X ^ N/2) % M * (X ^ N/2) % M) % M   ==> if N is even
+X ^ N % M = (X % M * (X ^ N-1) % M) % M   ==> if N is odd
+1 ==> N is 0
 """
