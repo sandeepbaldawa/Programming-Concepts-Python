ffffcc7879f3de012d52971fd4731a48d529f09f
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-11-02 21:06:20 -0800

    Create occur_more_than_one_third.py

---
+++ arrays/occur_more_than_one_third.py
@@ -0,0 +1,13 @@
+"""
+You’re given a read only array of n integers. Find out if any integer occurs more than n/3 times in the array in linear time and constant additional space.
+
+If so, return the integer. If not, return -1.
+
+If there are multiple solutions, return any one.
+
+Example :
+
+Input : [1 2 3 1 1]
+Output : 1 
+1 occurs 3 times which is more than 5/3 times. 
+"""
