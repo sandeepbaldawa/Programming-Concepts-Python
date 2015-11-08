c96abd4b8fcfa3e9758d554824420fc1dbee21fe
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-11-08 00:03:15 -0800

    Update first_missing_positive.py

--- arrays/first_missing_positive.py
+++ arrays/first_missing_positive.py
@@ -4,19 +4,29 @@ For example,
 Given [1,2,0] return 3,
 and [3,4,-1,1] return 2.
 Your algorithm should run in O(n) time and uses constant space.
-"""
 
-"""
-Idea
-------
-The idea is as follow:
-e.g. 3,2,5,1,7
-For each element,
-1. Do not consider the element which <=0, or value > length n
-2. Swap this element A[i] with the A[A[i]-1], e.g. when we met A[0]=3, we swap A[0] and A[2], then A[0]=5
-3. For the current element A[i], if A[i]<=0 or A[i]>length n, or A[i] has already occurred, break. Else go to step 2.
-    e.g. A[0]=5, swap A[0] and A[5], A[0]=7, break.Now the array A is {7,2,3,1,5}.
-    Next round, A is {1,2,3,7,5}
-4. Scan the array A and find the result. e.g. {1,2,3,7,5}, the A[3]!=3+1, print 3+1, result =4.
+
+Note: numbers A[i]<=0 and A[i]>N ( N being the size of the array ) is not important to us since the missing positive integer will be in the range [1, N+1].
+
+The answer will be N+1 only if all of the elements of the array are exact one occurrence of [1, N].
+
+Creating buckets would have been an easy solution if using extra space was allowed.
+
+An array of size N initialized to 0 would have been created.
+
+For every value A[i] which lies in the range [1, N], its count in the array would have been incremented.
+
+Finally, traversing the array would help to find the first array position with value 0 and that will be our answer. 
+However, usage of buckets is not allowed, can we use the existing array as bucket somehow?
+
+Now, since additional space is not allowed either, the given array itself needs to be used to track it.
+
+It may be helpful to use the fact that the size of the set we are looking to track is [1, N]
+
+which happens to be the same size as the size of the array.
+
+This means we can use the array to track these elements.
+
+We traverse the array and if A[i] is in [1,N] range, we try to put in the index of same value in the array.
 """
 
