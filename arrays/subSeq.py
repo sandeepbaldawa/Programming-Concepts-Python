abc1cdbf001fbfe803034a86810981cc969f8c2b
sandeepbaldawa <sandeepbaldawa@gmail.com>
2015-11-07 22:47:01 -0800

    Update subSeq.py

--- arrays/subSeq.py
+++ arrays/subSeq.py
@@ -38,3 +38,25 @@ def maxContiguousSubArray(arr):
 
 arr = [-2,1,-3,4,-1,2,1,-5,4]   
 maxContiguousSubArray(arr)    
+
+ /**
+     * CREDIT: InterviewBit.com
+     *
+     * Find out the maximum sub-array of non negative numbers from an array.
+     * The sub-array should be continuous.That is, a sub-array created by choosing
+     * the second and fourth element and skipping the third element is invalid.
+     * Maximum sub-array is defined in terms of the sum of the elements in the sub-array.
+     * Sub-array A is greater than sub-array B if sum(A) > sum(B).
+     *
+     * Example:
+     * A : [1, 2, 5, -7, 2, 3]
+     * The two sub-arrays are [1, 2, 5] [2, 3].
+     * The answer is [1, 2, 5] as its sum is larger than [2, 3]
+     * NOTE: If there is a tie, then compare with segment's length and return segment which has maximum length
+     * NOTE 2: If there is still a tie, then return the segment with minimum starting index
+     *
+     * @param input input array
+     *
+     * @return the maximum sub-array of non-negative numbers
+     *
+     */
