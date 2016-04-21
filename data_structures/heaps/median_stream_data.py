b90f47bcef155d6c97bafa23e67445c60c314472
sandeepbaldawa <sandeepbaldawa@gmail.com>
2016-04-20 19:39:25 -0700

    Create median_stream_data.py

---
+++ data_structures/heaps/median_stream_data.py
@@ -0,0 +1,9 @@
+Given that integers are read from a data stream. Find median of elements read so for in efficient way. For simplicity assume 
+there are no duplicates. For example, let us consider the stream 5, 15, 1, 3 …
+
+After reading 1st element of stream - 5 -> median - 5
+After reading 2nd element of stream - 5, 15 -> median - 10
+After reading 3rd element of stream - 5, 15, 1 -> median - 5
+After reading 4th element of stream - 5, 15, 1, 3 -> median - 4, so on...
+
+http://www.geeksforgeeks.org/median-of-stream-of-integers-running-integers/
