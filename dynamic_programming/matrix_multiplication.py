4b4a8c40fa0213bed9a59dafebc7868b11abd0e3
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-03-20 09:19:30 -0700

    Update matrix_multiplication.py

--- dynamic_programming/matrix_multiplication.py
+++ dynamic_programming/matrix_multiplication.py
@@ -2,3 +2,12 @@
 - Number of scalar operations to multiply are p*q*r
 - Size of resulting matrix is p*r (rows and columns)
 - Order in which we multiply multiple matrices can optimize the number of scalar operations required
+- Number of ways to do matrix multiplication is (2*(n-1))!/((n)!*(n-1)!)
+eg:- n=4
+    (2 * 3)! /((4!) * 3!)= 6 * 5 / 6 = 5
+    
+    n=3 => 2 ways
+    n=4 => 5 ways
+    n=5 => 14 ways 
+    
+    
