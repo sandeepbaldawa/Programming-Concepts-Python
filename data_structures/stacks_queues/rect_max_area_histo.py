23a87f3e39c1668b43d4db3b08394ea35dbed296
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-03-30 20:58:21 -0700

    Create rect_max_area_histo.py

---
+++ data_structures/stacks_queues/rect_max_area_histo.py
@@ -0,0 +1,13 @@
+"""
+1. Add to stack if current value is equal or bigger than top of the stack.
+2. Else keep removing from the stack till a number smaller than or equal to current value is found
+3. Everytime you remove calculate the area
+if stack is empty:
+    area = input[top] * i
+else:
+    area = input[top] * (i-stacktop-1)
+    
+    
+i => Jut the index of the bar item in histo    
+
+"""
