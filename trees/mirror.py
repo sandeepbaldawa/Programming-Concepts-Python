192280c223994d0dc3d9496385bc36bd7a872a95
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-04-29 23:10:21 -0700

    Update mirror.py

--- trees/mirror.py
+++ trees/mirror.py
@@ -4,9 +4,11 @@
   2    5   ==    5     2
 1  3                3    1
 '''
-def mirror_tree(node):
-    if not node return
-    tmp = Node(node.val)
-    tmp_right = mirror_tree(tmp_left)
-    tmp_left = mirror_tree(tmp_right)
-    return tmp
+def mirror(n):
+    if !n:
+       return
+     nleft = traverse(n.left)
+     nright = traverse(n.right)
+     n.left = nright
+     n.right = nleft
+     return n
