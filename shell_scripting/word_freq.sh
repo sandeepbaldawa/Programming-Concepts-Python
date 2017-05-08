c040fd09ee27f6e70806d6097c8af71b6617067f
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-05-07 21:31:18 -0700

    Update word_freq.sh

--- shell_scripting/word_freq.sh
+++ shell_scripting/word_freq.sh
@@ -1,44 +1,20 @@
-#!/bin/bash
-# wf2.sh: Crude word frequency analysis on a text file.
+'''
+Write a bash script to calculate the frequency of each word in a text file words.txt.
 
-# Uses 'xargs' to decompose lines of text into single words.
-# Compare this example to the "wf.sh" script later on.
+For simplicity sake, you may assume:
 
+words.txt contains only lowercase characters and space ' ' characters.
+Each word must consist of lowercase characters only.
+Words are separated by one or more whitespace characters.
+For example, assume that words.txt has the following content:
 
-# Check for input file on command line.
-ARGS=1
-E_BADARGS=65
-E_NOFILE=66
+the day is sunny the the
+the sunny is is
+Your script should output the following, sorted by descending frequency:
+the 4
+is 3
+sunny 2
+day 1
 
-if [ $# -ne "$ARGS" ]
-# Correct number of arguments passed to script?
-then
-  echo "Usage: `basename $0` filename"
-  exit $E_BADARGS
-fi
-
-if [ ! -f "$1" ]       # Check if file exists.
-then
-  echo "File \"$1\" does not exist."
-  exit $E_NOFILE
-fi
-
-
-
-########################################################
-cat "$1" | xargs -n1 | \
-#  List the file, one word per line. 
-tr A-Z a-z | \
-#  Shift characters to lowercase.
-sed -e 's/\.//g'  -e 's/\,//g' -e 's/ /\
-/g' | \
-#  Filter out periods and commas, and
-#+ change space between words to linefeed,
-sort | uniq -c | sort -nr
-#  Finally prefix occurrence count and sort numerically.
-########################################################
-
-#  This does the same job as the "wf.sh" example,
-#+ but a bit more ponderously, and it runs more slowly (why?).
-
-exit 0
+'''
+tr -s ' ' '\n' < words.txt|sort|uniq -c|sort -nr|awk '{print $2, $1}'
