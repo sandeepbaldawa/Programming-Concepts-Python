fc56004ea27f0499701f21c79c5a35a60dff9d40
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-05-22 11:44:03 -0700

    Update find_all_path.py

--- recursion/find_all_path.py
+++ recursion/find_all_path.py
@@ -3,6 +3,8 @@ Find paths to go from 0,0 to n-1, n-1 in a square grid
 using unoptimized recursion
 This can be further improved by memoization and even further improved by 
 bottom up approach using DP..
+
+Complexity O(2^N)
 '''
 
 def allpaths(x, y, n):
@@ -18,3 +20,24 @@ def allpaths(x, y, n):
    return m1 + m2
 
 print allpaths(0, 0, 4)
+
+
+'''
+Using DP 
+
+Complexity O(N^2)
+'''
+def numOfPathsToDest(n):
+    if n <= 1:
+      return n
+    a = [[0]*(n+1) for _ in range(n+1)]
+    for i in range(n+1):
+       a[0][i] = 1
+
+    for i in range(n+1):
+       for j in range(n+1):
+          if i >= j and i > 0 and j > 0:
+             a[i][j] = a[i-1][j] + a[i][j-1]
+    return a[n][n]
+
+print numOfPathsToDest(4)
