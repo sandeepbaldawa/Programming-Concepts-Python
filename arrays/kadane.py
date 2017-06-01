eecd742f327df72dd67a0a1e1e356e2f03341c66
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-06-01 00:13:06 -0700

    Update kadane.py

--- arrays/kadane.py
+++ arrays/kadane.py
@@ -9,12 +9,25 @@ curr_max = max(curr_val_in arr, curr_max + curr_val_in arr)
 '''
 
 def Kadane(arr):
+
    max_curr = arr[0]
    max_global = arr[0]
+   begin_idx , end_idx = 0, 0
    for idx in range(1, len(arr)):
       max_curr = max(arr[idx], max_curr + arr[idx])
+
+      if max_curr == arr[idx]:
+         begin_idx = idx
+
       max_global = max(max_curr, max_global)
-   return max_global
+
+      if max_global == max_curr:
+        end_idx = idx
+
+   return max_global, begin_idx, end_idx
+arr = [-2, -3, 4]
+print arr
+print Kadane(arr)
 
 
 arr = [-2, -3, 4, -1, -2, 1, 5, -3]
