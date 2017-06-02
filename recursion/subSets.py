792ad7463eaeb8a24f21894548de20791ff097be
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-06-01 17:28:32 -0700

    Update subSets.py

--- recursion/subSets.py
+++ recursion/subSets.py
@@ -9,3 +9,20 @@ def getSubSets(soFar, rest):
     getSubSets(soFar, rest[1:])           # Or dont choose the character and recurse
 
 getSubSets("","abc")
+
+
+# Second method, instead of creating new arrays pass around the indice
+def sub(word, word_idx, soFar, soFar_idx):
+  if word_idx == len(soFar):
+    print(soFar[:soFar_idx])
+    return
+
+  sub(word, word_idx+1, soFar, soFar_idx)
+  soFar[soFar_idx] = word[word_idx]
+  sub(word, word_idx+1, soFar, soFar_idx+1)
+
+word = 'abc'
+soFar = [0] * len(word)
+print sub(word, 0, soFar, 0)
+
+
