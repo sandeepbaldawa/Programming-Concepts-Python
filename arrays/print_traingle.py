b66239e7797ec52357ff487d917d37a1cd7178e0
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-06-05 10:00:17 -0700

    Create print_traingle.py

---
+++ arrays/print_traingle.py
@@ -0,0 +1,32 @@
+# write a function which takes one integer parameter,
+# and print in such fashion:
+# 1
+# 2 3
+# 4 5 6
+# ... n, where n is the input parameter
+
+#n => 0..
+def printPattern(n):
+    if n <= 0:
+        print "0"
+        return
+    line_size = 1
+    num = 1
+    while(True):
+        string = ""
+        # print how many numbers on each line
+        for i in range(line_size):
+            if num >= n:
+                string = string + str(num)
+                return
+            string = string + str(num) + " "
+            num += 1
+        print string    
+        line_size += 1
+        
+            
+#printPattern(7)
+#printPattern(0)
+#printPattern(7)
+#printPattern(106)
+#printPattern(3)
