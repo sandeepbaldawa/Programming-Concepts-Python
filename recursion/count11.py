669d941788f767151c1fbfea2e7920d54352f3c1
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-06-08 06:32:31 -0700

    Update count11.py

--- recursion/count11.py
+++ recursion/count11.py
@@ -4,26 +4,32 @@ compute recursively (no loops) the number of "11" substrings in the string.
 The "11" substrings should not overlap.
 """
 
-def count11(str, count=0):
-    if len(str) <= 1:
-        print "Count is ",count
-        return
-    
-    if str[0:2] == "11":
-        count = count + 1
-    elif str[1] and str[1] == "1":
-        return count11(str[1:], count)
+def count11(str, count=0, idx=0):
+  if idx >= (len(str)):
+    print count
+    return
 
-    return count11(str[2:], count)
-    
-count11("11abc11")	    
-count11("abc11x11x11")    
-count11("111") 	    
-count11("1111") 	    
-count11("1") 	    
-count11("") 
-count11("hi")    
-count11("11x111x1111")	    
-count11("1x111") 	    
-count11("1Hello1") 	    
-count11("Hello") 
+  if str[idx:idx+2] == "11":
+    count += 1
+  elif str[idx:idx+1] == "1":
+    return count11(str, count, idx + 1)
+  elif str[idx+1:idx+2] == "1":
+    return count11(str, count, idx + 1)
+  return count11(str, count, idx + 2)
+
+
+
+
+
+count11("11abc11")
+count11("abc11x11x11")
+count11("111")
+count11("1111")
+count11("1")
+count11("")
+count11("hi")
+count11("11x111x1111")
+count11("1x111")
+count11("1Hello1")
+count11("Hello")
+~                  
