d4c43175221365dc58d1e7115e0d509a7aacd5f5
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-06-08 05:51:31 -0700

    Create swap_bits.py

--- bitwsei/swap_bits.py
+++ bitwsei/swap_bits.py
@@ -1,7 +1,7 @@
 #Below just toggles the bits when we know the bits are different..
 def swap_bits(x, i, j):
     # Toggle bits only if both ith and jth value are diff
-    if ((x >> i) & 1) != ((j >> i) & 1):
+    if ((x >> i) & 1) != ((x >> j) & 1):
         bit_mask =  ((1 << i) | (1<< j))
         x = x ^ bit_mask
 
