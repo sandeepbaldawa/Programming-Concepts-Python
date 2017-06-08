a936f71d15a2d3a2c47cbfda64162f279e1f171a
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-06-08 06:05:03 -0700

    Create bracket_pairs.py

--- recursion/bracket_pairs.py
+++ recursion/bracket_pairs.py
@@ -4,13 +4,8 @@ def bracket_pairs(l, r, curr):
     if l == 0 and r == 0:
         print "====== ACTUAL PRINTING ======", curr
     if l > 0:
-        print "ENTERING LEFT ===>",l, r, curr
         bracket_pairs(l-1, r , curr + "(")
-        print "EXITING LEFT ===>",l, r, curr
     if r > 0 and r > l:
-        print "ENTERING RIGHT ===>",l, r, curr
-        bracket_pairs(l, r-1, curr + ")")
-        print "EXITING RIGHT ===>",l, r, curr
-
+        bracket_pairs(l, r-1, curr + ")")   
 
 bracket_pairs(2,2,"")
