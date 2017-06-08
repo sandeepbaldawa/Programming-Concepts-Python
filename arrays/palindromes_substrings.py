e60da64829d934b3928da463ddf1d4396261d445
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-06-08 08:19:42 -0700

    Create palindromes_substrings.py

--- arrays/palindromes_substrings.py
+++ arrays/palindromes_substrings.py
@@ -21,7 +21,7 @@ def getPalindromesOptimized(input):
    count = 0
    for i in range(len(input)):
       for j in range(len(input)):
-         if (i-j) < 0 or ((i+j) == len(input)):
+         if (i-j) < 0 or ((i+j) >= len(input)):
            break
          if input[i+j] != input[i - j]:
             break
@@ -30,7 +30,7 @@ def getPalindromesOptimized(input):
 
    for i in range(len(input)):
       for j in range(len(input)):
-         if (i-j) < 0 or ((i+j+1) == len(input)):
+         if (i-j) < 0 or ((i+j+1) >= len(input)):
            break
          if input[i+j+1] != input[i - j]:
             break
