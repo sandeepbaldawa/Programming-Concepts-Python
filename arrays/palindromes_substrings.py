e56ee1d6c536fdd83324a13bc551e671d9d035f5
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-06-12 14:33:48 -0700

    Create palindromes_substrings.py

--- arrays/palindromes_substrings.py
+++ arrays/palindromes_substrings.py
@@ -41,4 +41,56 @@ def getPalindromesOptimized(input):
 input = "abbbba"
 print getPalindromes(input)
 print getPalindromesOptimized(input)
-~                                     
+
+# --------------
+# User Instructions
+#
+# Write a function, longest_subpalindrome_slice(text) that takes 
+# a string as input and returns the i and j indices that 
+# correspond to the beginning and end indices of the longest 
+# palindrome in the string. 
+#
+# Grading Notes:
+# 
+# You will only be marked correct if your function runs 
+# efficiently enough. We will be measuring efficency by counting
+# the number of times you access each string. That count must be
+# below a certain threshold to be marked correct.
+#
+# Please do not use regular expressions to solve this quiz!
+def longest_subpalindrome_slice(text):
+    if len(text) <= 1:
+        return (0,len(text))
+    items=[]
+    "Return (i, j) such that text[i:j] is the longest palindrome in text."
+    for start in range(len(text)):
+        for end in (start, start+1):
+            items.append(grow(text,start,end))
+    return max_diff(items)
+    
+def grow(text, start, end):
+    while (start > 0 and end < len(text) and text[start].lower() == text[end].lower()):
+        start -= 1
+        end += 1
+    return (start, end)    
+
+def max_diff(items):
+    maxsoFar = 0
+    s_i = e_i = 0
+    for s, e in items:
+        if e-s > maxsoFar:
+            s_i, e_i = s, e 
+            maxsoFar = e-s
+    return s_i, e_i    
+    
+def test():
+    L = longest_subpalindrome_slice
+    print L('racecar')
+    assert L('racecar') == (0, 6)
+    assert L('Racecar') == (0, 6)
+    assert L('RacecarX') == (0, 6)
+    assert L('Race carr') == (6, 9)
+    assert L('') == (0, 0)
+    
+
+print test()
