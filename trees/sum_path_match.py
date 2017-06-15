dcdab40c99ef3753cb2f8bc1c97be0a90ebd591d
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-06-15 09:30:31 -0700

    Create sum_path_match.py

---
+++ trees/sum_path_match.py
@@ -0,0 +1,8 @@
+def sum_path_match(tree, target):
+    if not tree:
+        return abs(target - tree.node) == 0
+
+    if abs(target - tree.node) == 0:
+        return True
+    return sum_path_match(tree.left, target - tree.node) or sum_path_match(tree.right, target - tree.node):
+    return False
