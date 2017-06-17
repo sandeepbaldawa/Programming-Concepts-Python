cb3faf718030d2a1834201797f50ed17e20cc909
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-06-17 11:55:55 -0700

    Update sum_left_leaves.py

--- trees/sum_left_leaves.py
+++ trees/sum_left_leaves.py
@@ -36,3 +36,36 @@ class Solution(object):
         
         res += self.sumOfLeftLeaves(root.right)    
         return res
+
+    
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        summ = [0] # initialize result
+     
+        # Use the above recursive fucntion to evaluate sum
+        self.leftLeavesSumRec(root, 0, summ)
+        
+        return summ[0]
+    
+    def leftLeavesSumRec(self, root, isLeft, summ):
+        if root is None:
+            return
+     
+        # Check whether this node is a leaf node and is left
+        if root.left is None and root.right is None and isLeft == True:
+            summ[0] += root.val
+ 
+        # Pass 1 for left and 0 for right
+        self.leftLeavesSumRec(root.left, 1, summ)
+        self.leftLeavesSumRec(root.right, 0, summ)
