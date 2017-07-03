7ad435590601a1992b5894187d2056ad92d316db
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-07-02 21:02:39 -0700

    Create hard_serialize_deserialize_btree.py

---
+++ lcode/fb/hard_serialize_deserialize_btree.py
@@ -0,0 +1,40 @@
+'''
+Pre-Order depth traversal
+Method1:-
+To make tree perfect make the leaf nodes NULL(or MAXINT)
+when we store the same in a file else we can't reconstruct the tree
+Method2:-
+Store any two among pre-order, inorder or postorder
+and reconstruct the tree
+'''
+
+import pickle
+import sys
+class Node():
+    def __init__(self, val):
+        self.left=self.right=None
+        self.val = val
+
+MAXINT = sys.maxint
+def serialize(root, stream):
+    '''Serialize the given Binary Tree with root '''
+    if not root:
+        stream.dump(MAXINT)
+        return
+    stream.dump(root.val)
+    serialize(root.left)
+    serialize(root.right)
+
+def deserialize(stream):
+    '''De-Serialize the given Binary Tree with given file stream '''
+    try:
+        data = pickle.load(stream)
+        if data == MAXINT:
+            return None
+        
+        root = Node(data)
+        root.left = deserialize(stream)
+        root.right = deserialize(stream)
+        return root
+    except pickle.UnpicklingError:
+        return None
