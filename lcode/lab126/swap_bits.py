8e3c0886604336a42acaa6d50de80d3f679cc9d8
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-07-03 21:06:59 -0700

    Update swap_bits.py

--- lcode/lab126/swap_bits.py
+++ lcode/lab126/swap_bits.py
@@ -1,2 +1,10 @@
+'''
 write a program tto see is it is a prime or not
-bits manipulation...given: number 73 in binary, value of index i and j ...swap index i and j to get a new number.  
+bits manipulation...given: number 73 in binary, value of index i and j ...swap index i and j to get a new number. 
+'''
+#Below just toggles the bits when we know the bits are different..
+def swap_bits(x, i, j):
+    # Toggle bits only if both ith and jth value are diff
+    if ((x >> i) & 1) != ((x >> j) & 1):
+        bit_mask =  ((1 << i) | (1<< j))
+        x = x ^ bit_mask
