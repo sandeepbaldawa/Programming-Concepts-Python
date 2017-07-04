615ea26306ea0e518d6c6f6d7b934df5b8cb4ab3
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-07-03 17:37:31 -0700

    Create hard_regex_matching.py

---
+++ lcode/fb/hard_regex_matching.py
@@ -0,0 +1,32 @@
+'''Recursive'''
+class Solution:
+    # @return a boolean
+    def isMatch(self, s, p):
+        if len(p)==0: return len(s)==0
+        if len(p)==1 or p[1]!='*':
+            if len(s)==0 or (s[0]!=p[0] and p[0]!='.'):
+                return False
+            return self.isMatch(s[1:],p[1:])
+        else:
+            i=-1; length=len(s)
+            while i<length and (i<0 or p[0]=='.' or p[0]==s[i]):
+                if self.isMatch(s[i+1:],p[2:]): return True
+                i+=1
+            return False
+    
+
+'''DP'''
+class Solution:
+    # @return a boolean
+    def isMatch(self, s, p):
+        if len(p)==0: return len(s)==0
+        if len(p)==1 or p[1]!='*':
+            if len(s)==0 or (s[0]!=p[0] and p[0]!='.'):
+                return False
+            return self.isMatch(s[1:],p[1:])
+        else:
+            i=-1; length=len(s)
+            while i<length and (i<0 or p[0]=='.' or p[0]==s[i]):
+                if self.isMatch(s[i+1:],p[2:]): return True
+                i+=1
+            return False
