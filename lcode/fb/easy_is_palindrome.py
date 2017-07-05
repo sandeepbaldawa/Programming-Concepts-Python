a64f83f0a55a5eec58d45770dc2a4ec6b3cc41d2
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-07-04 23:42:55 -0700

    Create easy_is_palindrome.py

---
+++ lcode/fb/easy_is_palindrome.py
@@ -0,0 +1,30 @@
+'''
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.
+
+'''
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l +=1; r -= 1
+        return True
