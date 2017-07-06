5930f9c3d243bf5a8d1fdfe496e97b536d193e2c
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-07-05 22:27:30 -0700

    Create caching_example.py

---
+++ System-Design/Storage Scalability/web_architecture/caching_example.py
@@ -0,0 +1,26 @@
+import time
+
+# complex_computation() simulates a slow function. time.sleep(n) causes the
+# program to pause for n seconds. In real life, this might be a call to a
+# database, or a request to another web service.
+def complex_computation(a, b):
+    time.sleep(.5)
+    return a + b
+
+# QUIZ - Improve the cached_computation() function below so that it caches
+# results after computing them for the first time so future calls are faster
+cache = {}
+def cached_computation(a, b):
+    if (a,b) in cache.iteritems():
+        return cache[(a,b)]
+    else:
+        cache[(a,b)] = complex_computation(a, b)
+        return cache[(a,b)]
+
+start_time = time.time()
+print cached_computation(5, 3)
+print "the first computation took %f seconds" % (time.time() - start_time)
+
+start_time2 = time.time()
+print cached_computation(5, 3)
+print "the second computation took %f seconds" % (time.time() - start_time2)
