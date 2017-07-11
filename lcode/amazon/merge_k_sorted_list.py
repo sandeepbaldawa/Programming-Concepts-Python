b83c9f747c40e07c6925405cd02949f71571f71b
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-07-11 07:56:37 -0700

    Update merge_k_sorted_list.py

--- lcode/amazon/merge_k_sorted_list.py
+++ lcode/amazon/merge_k_sorted_list.py
@@ -1,4 +1,41 @@
 '''
 Merge k sorted linked lists and return it as one sorted list.
+
 '''
 
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):		
+        if self:		
+            return "{} -> {}".format(self.val, self.next)
+          
+     class Solution:
+    # @param a list of ListNode
+    # @return a ListNode
+    def mergeKLists(self, lists):
+        def mergeTwoLists(l1, l2):
+            curr = dummy = ListNode(0)
+            while l1 and l2:
+                if l1.val < l2.val:
+                    curr.next = l1
+                    l1 = l1.next
+                else:
+                    curr.next = l2
+                    l2 = l2.next
+                curr = curr.next
+            curr.next = l1 or l2
+            return dummy.next
+    
+        def mergeKListsHelper(lists, begin, end):
+            if begin > end:
+                return None
+            if begin == end:
+                return lists[begin]
+            return mergeTwoLists(mergeKListsHelper(lists, begin, (begin + end) / 2), \
+                                 mergeKListsHelper(lists, (begin + end) / 2 + 1, end))
+   
+        return mergeKListsHelper(lists, 0, len(lists) - 1)     
