c8c77cdaccd09c7a898a1949896e1661707f89be
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-07-11 13:56:30 -0700

    Create medium_add_two_nos.py

---
+++ lcode/amazon/medium_add_two_nos.py
@@ -0,0 +1,28 @@
+'''
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+
+'''
+
+class Solution:
+# @return a ListNode
+ def addTwoNumbers(self, l1, l2):
+    carry = 0
+    dummy = n = ListNode(0)
+    while l1 or l2 or carry:
+        v1 = v2 = 0
+        if l1:
+            v1 = l1.val
+            l1 = l1.next
+        if l2:
+            v2 = l2.val
+            l2 = l2.next
+        carry, val = divmod(v1+v2+carry, 10)
+        n.next = ListNode(val)
+        n = n.next
+    return dummy.next
