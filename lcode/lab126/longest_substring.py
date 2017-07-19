efd51f129a2dd73b78086a3d7dfc85c700ee27a7
sandeepbaldawa <sandeepbaldawa@gmail.com>
2017-07-18 19:33:05 -0700

    Update longest_substring.py

--- lcode/lab126/longest_substring.py
+++ lcode/lab126/longest_substring.py
@@ -1,4 +1,16 @@
 '''
 longest substring in two strings
 '''
-http://www.geeksforgeeks.org/longest-common-substring/
+def longest_common_substring(s1, s2):
+   m = [[0] * (1 + len(s2)) for i in xrange(1 + len(s1))]
+   longest, x_longest = 0, 0
+   for x in xrange(1, 1 + len(s1)):
+       for y in xrange(1, 1 + len(s2)):
+           if s1[x - 1] == s2[y - 1]:
+               m[x][y] = m[x - 1][y - 1] + 1
+               if m[x][y] > longest:
+                   longest = m[x][y]
+                   x_longest = x
+           else:
+               m[x][y] = 0
+   return s1[x_longest - longest: x_longest]
