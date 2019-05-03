2d42b12bce28910a0f3921cf45c0d14c4c556550
sandeepbaldawa <sandeepbaldawa@gmail.com>
2019-05-03 04:53:34 -0700

    Update LRUCache.py

--- data_structures/linked_lists/LRUCache.py
+++ data_structures/linked_lists/LRUCache.py
@@ -2,6 +2,27 @@
 # the same in cache
 # B. Ordering is important to know the Least recently used node
 
+# Questions to Clarify:Q. What should we return if an entry wasn't found? A. Return null
+# 
+# Say we want to add an entry (memory block) A to the cache. If the cache is not full, we simply add the entry. 
+# If the cache is full, we need to make space. We remove the entry that was accessed 
+# least recently (lets call this LR).This makes space, and now we can put A into the cache.
+# We need to store cache entries in order of when they were accessed. So if entry X was read a second ago,it should be 
+# at the front of our order. Same goes for entries that were added recently. If we keep blocks in this order, we can easily
+# find the least recently used (LRU) element. Itshould just be last on the list. It is also important to access each block 
+# quickly. It is a cache after all. So, we needa data structure that has the O(1) access of a hash table and keeps nodes 
+# in order.Remember, in a hash table, keys are not stored in order.A Linked Hash Table is the best data structure for this. 
+# It is a Linked List where each Nodeis stored in a Hash table as well.
+
+# The cache will have the following 2 standard 
+# operations:
+# 1. read(key)
+# 2. write(key, value). 
+# You should check with the interviewer if they want other operations. When we read an entry in the middle of the linked list, 
+# it becomes recently used. Sowe move it up to the front of the list. To move the node to front in O(1) time, we needto use a 
+# doubly linked list. This is because as we need to remove the node from the listbefore adding it to front. As we saw earlier, 
+# removal cannot be done in a singly linked list without the previous node.
+
 # 1. LinkedList finding a node is O(N)
 # 2. Order in HashTable is absent, but lookup is O(1)
 # 3. We can combine Linkedlists and HasTables to get the best of both worlds
