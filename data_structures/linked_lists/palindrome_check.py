7660298758ab9b0a736898849e2492860d8b1891
sandeepbaldawa <sandeepbaldawa@gmail.com>
2019-05-03 11:04:29 -0700

    Update palindrome_check.py

--- data_structures/linked_lists/palindrome_check.py
+++ data_structures/linked_lists/palindrome_check.py
@@ -1,1 +1,7 @@
 
+Is Palindrome: Given a Linked List, determine if it is a Palindrome. For example, the following lists are palindromes: 
+A -> B -> C -> B -> A 
+A -> B -> B -> A 
+K -> A -> Y -> A -> K
+
+Note: Can you do it with O(N) time and O(1) space? (Hint: Reverse a part of the list)
