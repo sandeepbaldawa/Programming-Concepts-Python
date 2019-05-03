568e28d02da35dbab8068dd715093e1d289009dd
sandeepbaldawa <sandeepbaldawa@gmail.com>
2019-05-03 10:56:13 -0700

    Update SmallestSubstringMatch.py

--- data_structures/linked_lists/SmallestSubstringMatch.py
+++ data_structures/linked_lists/SmallestSubstringMatch.py
@@ -1,1 +1,10 @@
 
+Smallest Subarray Covering All Values: Let's say you are given a large text document Doc. You are also given a set S of 
+words. You want to find the smallest substring of Doc thatcontains all the words in S. 
+For example:
+   S: ["and", "of", "one"]
+   Doc: "a set of words that is complete in itself, typically containing a subject and predicate,     conveying a statement, 
+         question, exclamation, or command, and consisting of a main clause and sometimes one or more subordinate clauses"
+          
+  "of a main clause and sometimes one" is the solution. Note that the order in which the words appear doesn't matter. 
+  Also, the length of the substring is in terms of number of characters.
