8ec4ecbfbc5bd9d8d43797cb6739e65dfcf2033c
sandeepbaldawa <sandeepbaldawa@gmail.com>
2019-05-03 11:08:44 -0700

    Update palindrome_check.py

--- data_structures/linked_lists/palindrome_check.py
+++ data_structures/linked_lists/palindrome_check.py
@@ -5,3 +5,7 @@ A -> B -> B -> A
 K -> A -> Y -> A -> K
 
 Note: Can you do it with O(N) time and O(1) space? (Hint: Reverse a part of the list)
+
+  Solution 1:- We can create a new list in reversed order and then compare each node. The time and space are O(n).
+  Solution 2:- We can use a fast and slow pointer to get the center of the list, then reverse the second list and compare two sublists. 
+               The time is O(n) and space is O(1).  
