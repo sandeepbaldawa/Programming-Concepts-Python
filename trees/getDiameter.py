eb87641a680a260299fc2e3466dace6f75a533c3
sandeepbaldawa <sandeepbaldawa@gmail.com>
2020-09-26 09:31:01 -0700

    Update getDiameter.py

--- trees/getDiameter.py
+++ trees/getDiameter.py
@@ -1,10 +1,19 @@
+# What is the diameter of a binary tree?
+# 1. First ask what does it mean? => Longest contiguous path in a tree, need not always go through the root
+# 2. Ask if it is nodes or edges
+# 3. Define function definition  
+#    func diameterOfBinaryTree(TreeNode root): int length
+# 4. Brute Force => Find all diameters and find the longest among them
+#    find max height on each left and right of a node 
+#    O(N^2)
+#  
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+#
 class Solution(object):
     def diameterOfBinaryTree(self, root):
         """
